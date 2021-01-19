8e96b8c6cb98366b538f4a76ee7babe691bf60dd
Jake Tener <jacobtener@gmail.com>
2021-01-18 21:08:35 -0600

    finised project v1

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,9 +1,15 @@
 import axios from 'axios'
+import { post } from 'request';
 import {setAlert} from './alert'
 import {
     GET_POSTS,
     POST_ERROR,
-    UPDATE_LIKES
+    UPDATE_LIKES,
+    DELETE_POST,
+    ADD_POST,
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from './types'
 
 export const getPosts = () => async dispatch => {
@@ -59,6 +65,119 @@ export const removeLike = postId => async dispatch => {
                 likes: res.data
             }
         })
+    } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {
+                msg: error.response.statusText,
+                status: error.response.status
+            }
+        })
+        
+    }
+}
+export const deletePost = postId => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/posts/${postId}`);
+
+        dispatch({
+            type: DELETE_POST,
+            payload: postId
+        })
+        dispatch(setAlert('Post Removed', 'success'))
+    } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {
+                msg: error.response.statusText,
+                status: error.response.status
+            }
+        })
+        
+    }
+}
+export const addPost = formData => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const res = await axios.post('/api/posts', formData, config);
+
+        dispatch({
+            type: ADD_POST,
+            payload: res.data
+        })
+        dispatch(setAlert('Post Created', 'success'))
+    } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {
+                msg: error.response.statusText,
+                status: error.response.status
+            }
+        })
+        
+    }
+}
+
+export const getPost = id => async dispatch => {
+    try {
+        const res = await axios.get(`/api/posts/${id}`);
+
+        dispatch({
+            type: GET_POST,
+            payload: res.data
+        })
+    } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {
+                msg: error.response.statusText,
+                status: error.response.status
+            }
+        })
+        
+    }
+}
+
+export const addComment = (postId, formData) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data
+        })
+        dispatch(setAlert('Comment Added', 'success'))
+    } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {
+                msg: error.response.statusText,
+                status: error.response.status
+            }
+        })
+        
+    }
+}
+
+export const deleteComment = (postId, commentId) => async dispatch => {
+    try {
+       
+        const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId
+        })
+        dispatch(setAlert('Comment Removed', 'success'))
     } catch (error) {
         dispatch({
             type: POST_ERROR,
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import Moment from 'react-moment'
 import { connect } from 'react-redux'
-import { addLike, removeLike} from '../../actions/post'
+import { addLike, removeLike, deletePost} from '../../actions/post'
 
-const PostItem = ({addLike, removeLike, auth, post: {_id, text, name, avatar, user, likes, comments, date}}) => {
+const PostItem = ({addLike, removeLike, deletePost, auth, post: {_id, text, name, avatar, user, likes, comments, date}, showActions = true}) => {
     return (
         <div className="post bg-white p-1 my-1">
           <div>
-            <a href="profile.html">
+            <Link to={`/profile/${user}`}>
               <img
                 className="round-img"
                 src={avatar}
                 alt=""
               />
               <h4>{name}</h4>
-            </a>
+            </Link>
           </div>
           <div>
             <p className="my-1">
@@ -25,27 +25,31 @@ const PostItem = ({addLike, removeLike, auth, post: {_id, text, name, avatar, us
              <p className="post-date">
                 Posted on <Moment format= "DD/MM/YYYY">{date}</Moment>
             </p>
-            <button type="button" onClick={e => addLike(_id)} className="btn btn-light">
-              <i className="fas fa-thumbs-up"></i>{' '}
-              {likes.length > 0 && (
-                  <span>{likes.length}</span>
-              )}
-            </button>
-            <button type="button" onClick={e => removeLike(_id)} className="btn btn-light">
-              <i className="fas fa-thumbs-down"></i>
-            </button>
-            <Link to={`/post/${_id}`} className="btn btn-primary">
-              Discussion {comments.length > 0 && (
-                  <span className='comment-count'>{comments.length}</span>
-                )}
-            </Link>
-            {!auth.loading && user === auth.user._id && (
-                <button      
-                type="button"
-                className="btn btn-danger">
-                <i className="fas fa-times"></i>
+            {showActions && 
+              <Fragment>
+                <button type="button" onClick={e => addLike(_id)} className="btn btn-light">
+                  <i className="fas fa-thumbs-up"></i>{' '}
+                  {likes.length > 0 && (
+                      <span>{likes.length}</span>
+                  )}
                 </button>
-            )}
+                <button type="button" onClick={e => removeLike(_id)} className="btn btn-light">
+                  <i className="fas fa-thumbs-down"></i>
+                </button>
+                <Link to={`/posts/${_id}`} className="btn btn-primary">
+                  Discussion {comments.length > 0 && (
+                      <span className='comment-count'>{comments.length}</span>
+                    )}
+                </Link>
+                {!auth.loading && user === auth.user._id && (
+                    <button      
+                    type="button"
+                    onClick={e=> deletePost(_id)}
+                    className="btn btn-danger">              
+                    <i className="fas fa-times"></i>
+                    </button>
+                )}
+              </Fragment>}
           </div>
         </div>
 
@@ -56,11 +60,12 @@ PostItem.propTypes = {
     auth: PropTypes.object.isRequired,
     post: PropTypes.object.isRequired,
     addLike: PropTypes.func.isRequired,
-    removeLike: PropTypes.func.isRequired
+    removeLike: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps, {addLike, removeLike })(PostItem)
+export default connect(mapStateToProps, {addLike, removeLike, deletePost})(PostItem)
