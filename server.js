f3d58282a7ec96b82fa00510e8be9692e15e77d7
Jake Tener <jacobtener@gmail.com>
2021-01-18 21:23:47 -0600

    deployment techs

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path')
 
 const app = express();
 // Connect DB
 connectDB();
-
-app.get('/', (req, res) => res.send('API Running'));
 
 //Init Middleware
 app.use(express.json({extended: false}));
@@ -18,6 +16,13 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client',  'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
